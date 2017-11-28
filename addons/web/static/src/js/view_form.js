595bf0652e086b33a3cd91f34b547ca1a4577786
Anton Chepurov <anton.chepurov@gmail.com>
2017-11-28 19:23:46 +0200

    search view: if field is no_create, disable Create in Search More... too

--- addons/web/static/src/js/view_form.js
+++ addons/web/static/src/js/view_form.js
@@ -3459,14 +3459,19 @@ instance.web.form.CompletionFieldMixin = {
     _search_create_popup: function(view, ids, context) {
         var self = this;
         var pop = new instance.web.form.SelectCreatePopup(this);
+        var options = {
+            title: (view === 'search' ? _t("Search: ") : _t("Create: ")) + this.string,
+            initial_ids: ids ? _.map(ids, function (x) {
+                return x[0];
+            }) : undefined,
+            initial_view: view,
+            disable_multiple_selection: true
+        };
+        if (self.options['no_create'])
+            options = Object.assign(options, self.options);
         pop.select_element(
             self.field.relation,
-            {
-                title: (view === 'search' ? _t("Search: ") : _t("Create: ")) + this.string,
-                initial_ids: ids ? _.map(ids, function(x) {return x[0];}) : undefined,
-                initial_view: view,
-                disable_multiple_selection: true
-            },
+            options,
             self.build_domain(),
             new instance.web.CompoundContext(self.build_context(), context || {})
         );
