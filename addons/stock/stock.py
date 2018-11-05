d48f3806ca81f585f6d6c2f0725353ecca587475
Anton Chepurov <anton.chepurov@gmail.com>
2018-11-05 17:58:41 +0200

    workshop CL: chain moves Workshop => Runup using Workshop Route
    - chain moves: use procurement group to keep order quants together
    - workshop quants: send all quants from Last Move to Workshop. allows
    moving a separate finisher along with the kit through the Workshop.
    - workshop quants: allow adding/removing quants to send to WS
    - CL: on start/stop don't move quant to workshop/runup if already there
    - [FIX] odoo: pass context on to action_confirm() of chained moves

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -3854,7 +3854,7 @@ class stock_location_path(osv.osv):
             move_obj.write(cr, uid, [move.id], {
                 'move_dest_id': move_id,
             })
-            move_obj.action_confirm(cr, uid, [move_id], context=None)
+            move_obj.action_confirm(cr, uid, [move_id], context=context)
 
 
 # -------------------------
