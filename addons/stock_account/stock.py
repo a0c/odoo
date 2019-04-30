190f29136826fe0d7c0d862ddd321529419ef625
Anton Chepurov <anton.chepurov@gmail.com>
2019-04-30 11:14:19 +0300

    speedup DO->Inv: cache invoice_vals, product categ accounts (inc/exp)
    only compute them once, then take from cache
    
    odoo:
    - extract _get_account_invoice() to allow overriding it

--- addons/stock_account/stock.py
+++ addons/stock_account/stock.py
@@ -131,21 +131,26 @@ class stock_move(osv.osv):
                     return price
         return move_line.product_id.lst_price
 
+    def _get_account_invoice(self, inv_type, context):
+        if inv_type in ('out_invoice', 'out_refund'):
+            account_id = self.product_id.property_account_income.id
+            if not account_id:
+                account_id = self.product_id.categ_id.property_account_income_categ.id
+        else:
+            account_id = self.product_id.property_account_expense.id
+            if not account_id:
+                account_id = self.product_id.categ_id.property_account_expense_categ.id
+        return account_id
+
     def _get_invoice_line_vals(self, cr, uid, move, partner, inv_type, context=None):
         fp_obj = self.pool.get('account.fiscal.position')
         # Get account_id
         fp = fp_obj.browse(cr, uid, context.get('fp_id')) if context.get('fp_id') else False
         name = False
+        account_id = move._get_account_invoice(inv_type, context)
         if inv_type in ('out_invoice', 'out_refund'):
-            account_id = move.product_id.property_account_income.id
-            if not account_id:
-                account_id = move.product_id.categ_id.property_account_income_categ.id
             if move.procurement_id and move.procurement_id.sale_line_id:
                 name = move.procurement_id.sale_line_id.name
-        else:
-            account_id = move.product_id.property_account_expense.id
-            if not account_id:
-                account_id = move.product_id.categ_id.property_account_expense_categ.id
         fiscal_position = fp or partner.property_account_position
         account_id = fp_obj.map_account(cr, uid, fiscal_position, account_id)
 
