fa13212c58cfec7626938d50635125c22eb2ab00
Anton Chepurov <anton.chepurov@gmail.com>
2023-07-20 23:44:28 +0300

    [IMP] field: group_operator informs read_group() to load DB data

--- openerp/models.py
+++ openerp/models.py
@@ -2103,7 +2103,7 @@ class BaseModel(object):
             if f not in ('id', 'sequence')
             if f not in groupby_fields
             if f in self._fields
-            if self._fields[f].type in ('integer', 'float')
+            if self._fields[f].type in ('integer', 'float') or hasattr(self._fields[f], 'group_operator') and self._fields[f].group_operator
             if getattr(self._fields[f].base_field.column, '_classic_write', False)
         ]
 
